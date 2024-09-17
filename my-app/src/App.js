13f8dcfb35cf1097f2bf3e4eb80bd1d4fdc78708
vipulyadav503@gmail.com <vipulyadav503@gmail.com>
2024-09-17 18:47:12 +0530

    deployment testing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './component/Navigation';
-import './App.css';   // Adjust the path as needed
 import Home from './component/home';
 import Contact from './component/Contact';
 import AboutComponent from './component/AboutComponent';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from './component/Footer';
-
-
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
   return (
-    <>    
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/AboutComponent" element={<AboutComponent/>}/>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/home" element={<Home />} />
+        <Route path="/AboutComponent" element={<AboutComponent />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/legaladvice" element={<LegalAdvice />} /> */}
-        {/* <Route path="/ipcsection" element={<IpcSection/>} /> */}
-        
         {/* Add more routes here as needed */}
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
-    </>
-    
-
   );
 };
 
